55d5dbf78f722c917b32d78d728c6e9618e4c1ae
Карпов Егор Андреевич <karpov@myservices.digital>
2023-12-06 20:58:38 +0300

    1.2.2

---
+++ src/pages/api/v1/calendar/[date]/index.ts
@@ -0,0 +1,21 @@
+import {NextApiRequest, NextApiResponse} from "next";
+import {getItem} from "@/pages/api/v1/calendar/[date]/service";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'GET') {
+        const date = parseInt(req.query.date as string);
+        if (!date) {
+            res.status(400);
+            return;
+        }
+        const dateValue = new Date(date);
+        const [books, error] = await getItem(dateValue);
+        if (error) {
+            res.status(400);
+            return;
+        }
+        res.status(200).json({
+            data: books || null
+        });
+    }
+}
---
+++ src/pages/api/v1/calendar/[date]/service.ts
@@ -0,0 +1,51 @@
+import clientPromise from "@/lib/mongodb";
+import {ObjectId} from "mongodb";
+import {TextType} from "@/utils/texts";
+import {aggregationDayWithMonth, aggregationTextWithBook, getAggregationAddField} from "@/utils/database";
+
+export const getItem = async (date: Date) => {
+    try {
+        const client = await clientPromise;
+        const db = client.db("typikon");
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+
+        const days = await db
+            .collection("days")
+            .aggregate([
+                { $match: { monthIndex: { $eq: day },  } },
+                ...aggregationDayWithMonth,
+                ...aggregationTextWithBook,
+                // Для каждого типа поля нужна такая структура
+                getAggregationAddField(TextType.VESPERS_PROKIMENON),
+                getAggregationAddField(TextType.VIGIL),
+                getAggregationAddField(TextType.KATHISMA_1),
+                getAggregationAddField(TextType.KATHISMA_2),
+                getAggregationAddField(TextType.KATHISMA_3),
+                getAggregationAddField(TextType.IPAKOI),
+                getAggregationAddField(TextType.POLYELEOS),
+                getAggregationAddField(TextType.SONG_3),
+                getAggregationAddField(TextType.SONG_6),
+                getAggregationAddField(TextType.APOLUTIKA_TROPARIA),
+                getAggregationAddField(TextType.BEFORE_1h),
+                getAggregationAddField(TextType.PANAGIA),
+                getAggregationAddField(TextType.H1),
+                getAggregationAddField(TextType.H3),
+                getAggregationAddField(TextType.H6),
+                getAggregationAddField(TextType.H9),
+                { $match: { "month.value": { $eq: month } }},
+                {
+                    $addFields: {
+                        id: { $toString: "$_id" },
+                    },
+                },
+                { $project: { texts: 0, books: 0, months: 0, "month._id": 0, "month.days": 0, weekId: 0, _id: 0 }}
+            ])
+            .toArray();
+        console.log(days);
+        return [days[0], null];
+    } catch (e) {
+        console.error(e);
+        return [null, { error: "Ошибка"}];
+    }
+};
