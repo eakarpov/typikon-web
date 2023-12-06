ceafad3545b8172cafa871478129c3d5b3310710
Карпов Егор Андреевич <karpov@myservices.digital>
2023-12-06 21:40:27 +0300

    app 0.4.0

--- src/pages/api/v1/app/version.ts
+++ src/pages/api/v1/app/version.ts
@@ -3,7 +3,7 @@ import {NextApiRequest, NextApiResponse} from "next";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'GET') {
         console.log("get method");
-        res.status(200).json({ major: 0, minor: 3 });
+        res.status(200).json({ major: 0, minor: 4 });
     }
     console.log('not get method');
 }
