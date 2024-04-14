23ec58fcf3ccf5d3fdd171e0b140dae7a600c8a0
Карпов Егор Андреевич <karpov@myservices.digital>
2024-04-15 02:48:41 +0300

    remove headers call

--- src/lib/meta.ts
+++ src/lib/meta.ts
@@ -1,8 +1,8 @@
-import {headers} from "next/headers";
+// import {headers} from "next/headers";
 
 export const setMeta = () => {
-    const headersList = headers();
-    const fullUrl = headersList.get('referer') || "";
-    console.log(fullUrl);
+    // const headersList = headers();
+    // const fullUrl = headersList.get('referer') || "";
+    // console.log(fullUrl);
     // fetch(`/api/meta/log?source=${fullUrl}`);
 };
