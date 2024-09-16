adae9c3863f3877dc77161bcb1927b29b73a6466
Карпов Егор Андреевич <karpov@myservices.digital>
2024-09-16 19:25:57 +0300

    dneslov thumb url

--- src/app/reading/DneslovImages.tsx
+++ src/app/reading/DneslovImages.tsx
@@ -13,7 +13,7 @@ const DneslovImages = ({ dneslovId }: IDneslovImages) => {
             fetch(`https://dneslov.org/api/v1/images.json?m=${dneslovId}`)
                 .then((res) => res.json())
                 .then((res) => {
-                    setImages(res.map(e => e.url));
+                    setImages(res.map(e => e.thumb_url));
                 });
         }
     }, [dneslovId]);
