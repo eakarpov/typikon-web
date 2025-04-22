a0a8bd9b044ac261e7a10fc28a77f68e181fd0e8
Карпов Егор Андреевич <karpov@myservices.digital>
2025-04-22 17:33:57 +0300

    before 50 in calc

--- src/pages/api/calc/getTriodion.ts
+++ src/pages/api/calc/getTriodion.ts
@@ -52,6 +52,7 @@ export const getTriodicItem = async (searchTriodion: any) => {
                 getAggregationAddField(TextType.KATHISMA_1),
                 getAggregationAddField(TextType.KATHISMA_2),
                 getAggregationAddField(TextType.KATHISMA_3),
+                getAggregationAddField(TextType.BEFORE_50),
                 getAggregationAddField(TextType.IPAKOI),
                 getAggregationAddField(TextType.POLYELEOS),
                 getAggregationAddField(TextType.SONG_3),
