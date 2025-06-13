8110431d348137284640af163b0159692fcc8935
Карпов Егор Андреевич <karpov@myservices.digital>
2025-06-13 23:53:30 +0300

    cookie fix

---
+++ src/app/api/logout/route.ts
@@ -0,0 +1,18 @@
+import {decrypt, deleteSession} from "@/lib/authorize/sessions";
+import {redirect} from "next/navigation";
+import {cookies} from "next/headers";
+import {NextRequest, NextResponse} from "next/server";
+
+export async function POST(request: NextRequest) {
+    const cookieStore = await cookies()
+    const cookie = cookieStore.get('session')?.value;
+    const session = await decrypt(cookie);
+    cookieStore.delete('session');
+    await deleteSession(session);
+
+    redirect('/login');
+
+    return new NextResponse(null, {
+        status: 200,
+    });
+}
--- src/pages/api/logout-old/index.ts
+++ src/pages/api/logout-old/index.ts
@@ -1,11 +1,11 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import {decrypt, deleteSession} from "@/lib/authorize/sessions";
 import {redirect} from "next/navigation";
-import {cookies} from "next/headers";
+import {headers} from "next/headers";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
-        const cookieStore = await cookies()
+        const cookieStore = await headers()
         const cookie = cookieStore.get('session')?.value;
         const session = await decrypt(cookie);
         cookieStore.delete('session');
