42db1810b37459fa20c7827bc0f2564b31f778a7
Карпов Егор Андреевич <karpov@myservices.digital>
2025-09-16 22:23:00 +0300

    cache removed

--- src/lib/authorize/authorization.ts
+++ src/lib/authorize/authorization.ts
@@ -1,5 +1,4 @@
 import 'server-only'
-import {cache} from 'react';
 import { cookies } from 'next/headers'
 import { decrypt } from '@/lib/authorize/sessions';
 import {NextApiRequest} from "next";
@@ -7,7 +6,7 @@ import clientPromise from "@/lib/mongodb";
 import {ObjectId} from "mongodb";
 import {getUserByVKId} from "@/lib/authorize/users";
 
-export const verifySession = cache(async () => {
+export const verifySession = async () => {
     const cookie = (await cookies()).get('session')?.value;
     const session = await decrypt(cookie);
 
@@ -18,7 +17,7 @@ export const verifySession = cache(async () => {
     // maybe here go to db, get vk refresh token and try to update the access one
 
     return { isAuth: false };
-});
+};
 
 export const verifySessionBack = async (req: NextApiRequest, isAdmin?: boolean) => {
     const { session: token } = req.cookies;
