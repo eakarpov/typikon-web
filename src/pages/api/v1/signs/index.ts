24c779c14d50afd5a9c218c55a050026fd97d636
Карпов Егор Андреевич <karpov@myservices.digital>
2026-02-16 17:08:49 +0300

    signs api fix

--- src/pages/api/v1/signs/index.ts
+++ src/pages/api/v1/signs/index.ts
@@ -3,7 +3,7 @@ import {getItems} from "@/app/signs/api";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'GET') {
-        const months = await getItems();
+        const [months] = await getItems();
         res.status(200).json(months);
     }
 }
