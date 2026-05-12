dcf07cf4b94626850de42d8c18341c06664b9d30
Карпов Егор Андреевич <karpov@myservices.digital>
2026-05-12 22:12:52 +0300

    types for sqlite

--- src/lib/sqlite.ts
+++ src/lib/sqlite.ts
@@ -1,7 +1,6 @@
-import sqlite3 from 'sqlite3';
-// @ts-ignore
+// import sqlite3 from 'sqlite3';
 import Database from 'better-sqlite3';
-import { open } from 'sqlite';
+// import { open } from 'sqlite';
 import path from "path";
 
 export const init = async () => {
